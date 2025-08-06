da67ba8cacf4b713219a6911a106da1b599f6bc5
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-06 18:16:24 +0000

    feat: Add ElectronicItem class with relevant fields and constructor
    - Included Id, Name, Quantity, Brand, and WarrantyMonths fields
    
    - Implemented constructor to initialize all fields
    
    - Represents electronic products in warehouse inventory

---
+++ WarehouseApp/WarehouseApp/ElectronicItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApp
+{
+    public class ElectronicItem
+    {
+        public int Id;
+        public string Name;
+        public int Quantity;
+        public string Brand;
+        public int WarrantyMonths;
+
+        public ElectronicItem(int id, string name, int quantity, string brand, int warrantyMonths)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Quantity = quantity;
+            this.Brand = brand;
+            this.WarrantyMonths = warrantyMonths;
+        }
+    }
+}
